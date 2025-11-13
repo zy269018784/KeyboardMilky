8878379cab39de1bc0d82cfcf65bba7a3c47fd77
zhouyue <269018784@qq.com>
2025-11-13 21:40:21 +0800

    build remove qt

---
+++ build.bat
@@ -0,0 +1,5 @@
+cmake -S . -B ../KeyboardMilky_build -DCMAKE_BUILD_TYPE=Release  
+cmake --open  ../KeyboardMilky_build
+
+
+
--- build2.bat
+++
@@ -1,6 +0,0 @@
-cmake -S . -B ../../../BEngine_build/MilkyKeyboard_build -DCMAKE_BUILD_TYPE=Release  -DUSE_QT6=true  -DCMAKE_PREFIX_PATH="C:/Qt/6.7.3/msvc2022_64/lib/cmake"
-cmake --open  ../../../BEngine_build/MilkyKeyboard_build
-C:/Qt/6.7.3/msvc2022_64/bin/windeployqt.exe --qmldir ../../../BEngine_build/MilkyKeyboard_build   ../../../BEngine_build/MilkyKeyboard_build/MilkyKeyboard.exe
-
-
-

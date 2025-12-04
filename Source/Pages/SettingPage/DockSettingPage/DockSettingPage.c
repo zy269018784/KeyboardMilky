ff561e5486de3c672b42f4cd62a1ae5b2079247e
zhouyue <269018784@qq.com>
2025-12-04 19:09:29 +0800

    Switch 样式

--- Source/Pages/SettingPage/DockSettingPage/DockSettingPage.c
+++ Source/Pages/SettingPage/DockSettingPage/DockSettingPage.c
@@ -2,14 +2,36 @@
 #include "../SettingsPage.h"
 #include "Widgets/Widget.h"
 #include "API/API.h"
+#include "Application.h"
 void CreateSettingsDockPage(SettingsPage* Page)
 {
+    lv_obj_t *obj = NULL;
+
     Page->PageDock.Handle = CreateBase(Page->Handle, 0, 0, LV_PCT(100), LV_PCT(100), lv_color_hex3(0x0F0));
 
     Page->PageDock.LabelShowDock = CreateLabel(Page->PageDock.Handle, 6, 100, 100, 20, "Show Dock", lv_color_hex3(0xFF0));
     Page->PageDock.SwitchShowDock = CreateSwitch(Page->PageDock.Handle, 200, 100, 200, 20, lv_color_hex3(0xFF0));
+    obj = Page->PageDock.SwitchShowDock ;
+    lv_obj_set_style_radius(obj, 10, 0);
+    lv_obj_set_style_bg_color(obj, App->color1, LV_PART_MAIN);
+    lv_obj_set_style_bg_color(obj, App->color1, LV_PART_INDICATOR);  // 灰色
+    lv_obj_set_style_bg_color(obj, App->color1, LV_PART_INDICATOR | LV_STATE_CHECKED);  // 绿色
+    lv_obj_set_style_bg_color(obj, App->color2, LV_PART_KNOB | LV_STATE_CHECKED);
+    lv_obj_set_style_bg_color(obj, App->color3, LV_PART_KNOB);
+    lv_obj_set_style_border_color(obj, App->color2, 0);
+    lv_obj_set_style_border_width(obj, 2, 0);
+
     Page->PageDock.LabelAutoHideDock = CreateLabel(Page->PageDock.Handle, 6, 140, 200, 20, "Auto Hide Dock", lv_color_hex3(0xFF0));
     Page->PageDock.SwitchAutoHideDock = CreateSwitch(Page->PageDock.Handle, 200, 140, 200, 20, lv_color_hex3(0xFF0));
+    obj = Page->PageDock.SwitchAutoHideDock;
+    lv_obj_set_style_radius(obj, 10, 0);
+    lv_obj_set_style_bg_color(obj, App->color1, LV_PART_MAIN);
+    lv_obj_set_style_bg_color(obj, App->color1, LV_PART_INDICATOR);  // 灰色
+    lv_obj_set_style_bg_color(obj, App->color1, LV_PART_INDICATOR | LV_STATE_CHECKED);  // 绿色
+    lv_obj_set_style_bg_color(obj, App->color2, LV_PART_KNOB | LV_STATE_CHECKED);
+    lv_obj_set_style_bg_color(obj, App->color3, LV_PART_KNOB);
+    lv_obj_set_style_border_color(obj, App->color2, 0);
+    lv_obj_set_style_border_width(obj, 2, 0);
 
     ResetDockSettingPage(&Page->PageDock);
 }
--- Source/Pages/SettingPage/TimeSettingPage/TimeSettingPage.c
+++ Source/Pages/SettingPage/TimeSettingPage/TimeSettingPage.c
@@ -6,6 +6,8 @@
 #include "API/API.h"
 void CreateSettingsClockPage(SettingsPage* Page)
 {
+    lv_obj_t *obj = NULL;
+
     Page->PageClock.Handle = CreateBase(Page->Handle, 0, 0, LV_PCT(100), LV_PCT(100), lv_color_hex3(0x0F0));
 
     int y;
@@ -14,15 +16,43 @@ void CreateSettingsClockPage(SettingsPage* Page)
     y = 50;
     Page->PageClock.LabelAutoGetTime = CreateLabel(Page->PageClock.Handle, 6, y, 200, 20, "Resync Time", lv_color_hex3(0xFF0));
     Page->PageClock.SwitchAutoGetTime = CreateSwitch(Page->PageClock.Handle, 200, y, 200, 20, lv_color_hex3(0xFF0));
+    obj = Page->PageClock.SwitchAutoGetTime;
+    lv_obj_set_style_radius(obj, 10, 0);
+    lv_obj_set_style_bg_color(obj, App->color1, LV_PART_MAIN);
+    lv_obj_set_style_bg_color(obj, App->color1, LV_PART_INDICATOR);  // 灰色
+    lv_obj_set_style_bg_color(obj, App->color1, LV_PART_INDICATOR | LV_STATE_CHECKED);  // 绿色
+    lv_obj_set_style_bg_color(obj, App->color2, LV_PART_KNOB | LV_STATE_CHECKED);
+    lv_obj_set_style_bg_color(obj, App->color3, LV_PART_KNOB);
+    lv_obj_set_style_border_color(obj, App->color2, 0);
+    lv_obj_set_style_border_width(obj, 2, 0);
+
 
     y = 100;
     Page->PageClock.LabelHideTime = CreateLabel(Page->PageClock.Handle, 6, y, 200, 20, "Hide Time", lv_color_hex3(0xFF0));
     Page->PageClock.SwitchHideTime = CreateSwitch(Page->PageClock.Handle, 200, y, 200, 20, lv_color_hex3(0xFF0));
+    obj = Page->PageClock.SwitchHideTime;
+    lv_obj_set_style_radius(obj, 10, 0);
+    lv_obj_set_style_bg_color(obj, App->color1, LV_PART_MAIN);
+    lv_obj_set_style_bg_color(obj, App->color1, LV_PART_INDICATOR);  // 灰色
+    lv_obj_set_style_bg_color(obj, App->color1, LV_PART_INDICATOR | LV_STATE_CHECKED);  // 绿色
+    lv_obj_set_style_bg_color(obj, App->color2, LV_PART_KNOB | LV_STATE_CHECKED);
+    lv_obj_set_style_bg_color(obj, App->color3, LV_PART_KNOB);
+    lv_obj_set_style_border_color(obj, App->color2, 0);
+    lv_obj_set_style_border_width(obj, 2, 0);
 
 
     y = 200;
     Page->PageClock.LabeHideDate = CreateLabel(Page->PageClock.Handle, 6, y, 200, 20, "Hide Date", lv_color_hex3(0xFF0));
     Page->PageClock.SwitchHideDate = CreateSwitch(Page->PageClock.Handle, 200, y, 200, 20, lv_color_hex3(0xFF0));
+    obj = Page->PageClock.SwitchHideDate;
+    lv_obj_set_style_radius(obj, 10, 0);
+    lv_obj_set_style_bg_color(obj, App->color1, LV_PART_MAIN);
+    lv_obj_set_style_bg_color(obj, App->color1, LV_PART_INDICATOR);  // 灰色
+    lv_obj_set_style_bg_color(obj, App->color1, LV_PART_INDICATOR | LV_STATE_CHECKED);  // 绿色
+    lv_obj_set_style_bg_color(obj, App->color2, LV_PART_KNOB | LV_STATE_CHECKED);
+    lv_obj_set_style_bg_color(obj, App->color3, LV_PART_KNOB);
+    lv_obj_set_style_border_color(obj, App->color2, 0);
+    lv_obj_set_style_border_width(obj, 2, 0);
 
 
     ResetTimeSettingPage(&Page->PageClock);
